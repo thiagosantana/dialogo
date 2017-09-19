79b5508ff0308b50f24cbf811c4c4a1a4bd18c2f
Thiago Santana <thiago.santana@gmail.com>
2017-09-18 23:36:48 -0300

    test support

--- test/test.js
+++ test/test.js
@@ -1,8 +1,3 @@
-var assert = require("assert");
-describe("Array", () => {
-	describe("#indexOf()", () => {
-		it("should return -1 when the value is not present", () => {
-			assert.equal(-1, [1, 2, 3].indexOf(4));
-		});
-	});
-});
+let chai = require("chai");
+let should = chai.should();
+let app = require("../src/app.js");
